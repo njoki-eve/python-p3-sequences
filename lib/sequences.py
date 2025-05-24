dae982bf9a4b71523562e16446ee2d2f44a385b1
njoki-eve <njokieverlyne71@gmail.com>
2025-05-24 02:24:40 -0700

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        
+    elif length == 1:
+        print('[0]')
+        
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_num = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_num)
+        print(fib_sequence)
